9182241ba41d486d4d0a576c4f3b095a4cb4e8e3
Ashish Prajapati <ashisherc@gmail.com>
2018-04-04 23:58:37 +0530

    setup gh pages branch

--- vuejs-frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    lotteryFactoryAddress: "0x39fEfF8dfb40A1f21F0FFe61C257C390CfF4d039"
-}
--- vuejs-frontend/src/web3/Lottery.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import lotteryJson from '../contract/Lottery.json'
-
-var lottery;
-if (web3) {
-    lottery = new web3.eth.Contract(JSON.parse(lotteryJson.interface));
-}
-
-export default lottery;
